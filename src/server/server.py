fed9874c7f5ae725b161f0c6c60533b10939483c
Thomas Nixon <tomjnixon@googlemail.com>
2010-04-24 18:25:18 +0100

    Added code to keep track of connections.

--- src/server/server.py
+++ src/server/server.py
@@ -6,6 +6,16 @@ from id_generator import IdGeneratorMixin
 from shet import ShetProtocol
 
 class ShetServerProtocol(ShetProtocol):
+
+	def connectionMade(self):
+		self.factory.connections.append(self)
+		self.connected = True
+
+	def connectionLost(self, reason):
+		self.factory.connections.remove(self)
+		self.connected = False
+
+
 	def test_cb(self, msg):
 		print msg
 		return "you said: %s" % msg
@@ -14,7 +24,6 @@ class ShetServerProtocol(ShetProtocol):
 		d = self.send_command_with_callback("foo", "hello")
 		return d.addCallback(self.test_cb)
 		
-
 
 	def cmd_test_error(self):
 		raise Exception("Oh Shit!")
@@ -26,3 +35,6 @@ class ShetServerProtocol(ShetProtocol):
 
 class ShetServerFactory(Factory):
 	protocol = ShetServerProtocol
+
+	def __init__(self):
+		self.connections = []
