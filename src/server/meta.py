04752e099447cbd220c39cf4a89c7d0cb6b9362c
Thomas Nixon <tomjnixon@googlemail.com>
2010-08-19 18:39:41 +0100

    Converted '/meta/ls' to use the 'shet_action' decorator.

--- src/server/meta.py
+++ src/server/meta.py
@@ -1,17 +1,19 @@
-from shet.client import ShetClient
+from shet.client import ShetClient, shet_action
 
 
 class MetaShet(ShetClient):
+	
+	root = "/meta"
 	
 	def __init__(self, server):
 		ShetClient.__init__(self)
 		self.server = server
 		self.add_action("/meta/ls-r", self.ls_recursive)
-		self.add_action("/meta/ls", self.ls)
 	
 	
 	def ls_recursive(self, dir='/'):
 		return self.server.fs.list_dir(dir)
 	
+	@shet_action
 	def ls(self, dir='/'):
 		return self.server.fs.list_dir(dir, False)
