3604f3b51d166e073be9a89acf826e0219a7bea8
Thomas Nixon <tomjnixon@googlemail.com>
2010-08-24 12:57:44 +0100

    Added a persistent event client.
    This exposes the following actions:
      /persist
      |- add_event(name)
      |    Add an event at /persist/name
      |- raise_event(name)
      |    Raise event at /persist/name
      |- remove_event(name)
      |    Remove event at /persist/name

---
+++ src/clients/persist.py
@@ -0,0 +1,31 @@
+from shet.client import ShetClient, shet_action
+
+class Persist(ShetClient):
+	
+	root = "/persist/"
+	
+	def __init__(self):
+		ShetClient.__init__(self)
+		
+		self.events = {}
+	
+	
+	@shet_action("add_event")
+	def _add_event(self, name):
+		if name not in self.events:
+			self.events[name] = self.add_event(name)
+	
+	
+	@shet_action("remove_event")
+	def _remove_event(self, name):
+		self.remove_event(self.events[name])
+		del self.events[name]
+	
+	
+	@shet_action("raise_event")
+	def _raise_event(self, name, *args):
+		self.events[name](*args)
+
+
+if __name__ == "__main__":
+	Persist().run("localhost")
