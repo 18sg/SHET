d825cda8ca55cb634d80d3a2eae97f6df6f76adc
Thomas Nixon <tom@tomn.co.uk>
2011-02-07 23:34:38 +0000

    Added on_activity meta event.

--- src/server/meta.py
+++ src/server/meta.py
@@ -1,4 +1,5 @@
 from shet.client import ShetClient, shet_action
+from functools import partial
 
 
 class MetaShet(ShetClient):
@@ -8,6 +9,8 @@ class MetaShet(ShetClient):
 	def __init__(self, server):
 		ShetClient.__init__(self)
 		self.server = server
+		
+		# Various meta events.
 		self.server.fs.on_change = self.add_event("on_tree_change")
 		self.server.fs.on_raise = self.add_event("on_raise")
 		self.server.fs.on_eventcreated = self.add_event("on_eventcreated")
@@ -15,6 +18,16 @@ class MetaShet(ShetClient):
 		self.server.fs.on_set = self.add_event("on_set")
 		self.server.fs.on_get = self.add_event("on_get")
 		self.server.fs.on_call = self.add_event("on_call")
+		
+		# Add an event that aggregates the above events.
+		on_activity = self.add_event("on_activity")
+		self.watch_event("on_tree_change", partial(on_activity, "tree_change"))
+		self.watch_event("on_raise", partial(on_activity, "raise"))
+		self.watch_event("on_eventcreated", partial(on_activity, "eventcreated"))
+		self.watch_event("on_eventdeleted", partial(on_activity, "eventdeleted"))
+		self.watch_event("on_set", partial(on_activity, "set"))
+		self.watch_event("on_get", partial(on_activity, "get"))
+		self.watch_event("on_call", partial(on_activity, "call"))
 	
 	
 	@shet_action("ls-r")
