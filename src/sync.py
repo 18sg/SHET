9c117ad9b3355b95fdcb34928d51d1bf78e2e677
Thomas Nixon <tom@tomn.co.uk>
2013-01-02 02:22:25 +0000

    make_sync no longer swallows exceptions.
    This prompted a major refactor, so it's all good. It's hopefully a lot
    more understandable now!

--- src/sync.py
+++ src/sync.py
@@ -1,23 +1,50 @@
-from twisted.internet import reactor
 from twisted.internet import defer
-from shet.error import ShetException
+from functools import wraps
 
 __all__ = ["make_sync"]
 
 def make_sync(f):
+    """A decorator to turn a generator into a deferred function.
+    
+    To wait for a deferred value to resolve, the decorated function should
+    yield it; the result will be passed back when it's callbacks run, or an
+    exception will be raised when it's errbacks run. The last value to be
+    yielded will be the return value of the function. If an exception is raised
+    the errback will be triggered.
+    
+    Example:
+        @make_sync
+        def new_function_that_returns_deferred():
+            x = (yield deferred_int_returning_function())
+            yield x + 1
+    """
+    @wraps(f)
     def g(*args, **kwargs):
         finished = defer.Deferred()
-        gen = f(*args, **kwargs)
-
+        
+        try:
+            gen = f(*args, **kwargs)
+        except Exception, e:
+            finished.errback(e)
+            return finished
+        
+        # If the function did not return a generator, return straight away.
         if not (hasattr(gen, "next") and hasattr(gen, "send")):
             finished.callback(gen)
-
-        def callback(x):
+            return finished
+        
+        # Step forward in the generator code, using the supplied function and
+        # arguments, f(*args). This should return a Deferred (yielded by the
+        # generator); callbacks are added such that when this generator completes,
+        # the value is passed back to the generator, using either gen.send or
+        # gen.throw. If StopIteration is raised because the generator has finished,
+        # complete finished with the supplied x value; the last generated value. If
+        # another exception is raised by the generator, pass it to finished.
+        def step(x, f, *args):
             try:
-                d = gen.send(x)
+                d = f(*args)
                 if isinstance(d, defer.Deferred):
-                    d.addCallback(callback)
-                    d.addErrback(errback)
+                    d.addCallbacks(callback, errback)
                 else:
                     callback(d)
             except StopIteration:
@@ -25,22 +52,15 @@ def make_sync(f):
             except Exception, e:
                 finished.errback(e)
         
+        def callback(x):
+            step(x, gen.send, x)
+        
         def errback(e):
-            print e.value
-            gen.throw(ShetException(e.value))
-
-        try:
-            d = gen.next()
-            if isinstance(d, defer.Deferred):
-                d.addCallback(callback)
-                d.addErrback(errback)
-            else:
-                callback(d)
-        except StopIteration:
-            finished.callback(d)
-        except Exception, e:
-            finished.errback(e)
-
+            step(None, gen.throw, e.type, e.value)
+        
+        step(None, gen.next)
+        
         return finished
 
     return g
+
