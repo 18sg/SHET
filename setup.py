1aaf971f7270738bf416929c3d1cca41047b898b
Thomas Nixon <tom@tomn.co.uk>
2013-11-02 23:29:46 +0000

    First release.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 
 setup(
     name='SHET',
-    version='0.1',
+    version='1.0.1',
     description='SHET House Event Tunnelling',
-    author='The UHG Guys',
+    author='Thomas Nixon, Jonathan Heathcote',
+    author_email='shet@tomn.co.uk',
     url='http://github.com/18sg/SHET',
     package_dir = {'shet': 'src'},
     packages=['shet', 'shet.server', 'shet.client', 'shet.path'],
