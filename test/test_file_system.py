26daec4bc41cbd1d2dee0b7b1dfce4f6ecb02336
Thomas Nixon <tom@tomn.co.uk>
2013-11-06 00:12:36 +0000

    Yet more file system tests.

--- test/test_file_system.py
+++ test/test_file_system.py
@@ -58,3 +58,50 @@ class EdgeCaseTests(unittest.TestCase):
 		m = Mock()
 		self.fs.add("/foo/bar", m)
 		self.assertRaises(Exception, lambda: self.fs.add("/foo/bar/baz", "quux"))
+	
+	def test_get_deeper(self):
+		m = Mock()
+		self.fs.add("/foo/bar", m)
+		self.assertRaises(Exception, lambda: self.fs.get_node("/foo/bar/baz"))
+
+
+class ComplexTests(unittest.TestCase):
+	
+	def setUp(self):
+		self.fs = FileSystem()
+	
+	def test_same_dir_add_remove(self):
+		foo = Mock()
+		bar = Mock()
+		
+		self.fs.add("/baz/foo", foo)
+		self.fs.add("/baz/bar", bar)
+		
+		self.assertEqual(self.fs.list_dir(),
+		    {'baz': {'foo': foo.type, 'bar': bar.type}})
+		
+		self.fs.remove("/baz/foo")
+		self.assertEqual(self.fs.list_dir(),
+		    {'baz': {'bar': bar.type}})
+		
+		self.fs.remove("/baz/bar")
+		self.assertEqual(self.fs.list_dir(),
+		    {})
+	
+	def test_add_remove_with_sub_dir(self):
+		foo = Mock()
+		baz = Mock()
+		
+		self.fs.add("/dir/foo", foo)
+		self.fs.add("/dir/bar/baz", baz)
+		
+		self.assertEqual(self.fs.list_dir(),
+		    {'dir': {'foo': foo.type, 'bar': {'baz': baz.type}}})
+		
+		self.fs.remove("/dir/foo")
+		self.assertEqual(self.fs.list_dir(),
+		    {'dir': {'bar': {'baz': baz.type}}})
+		
+		self.fs.remove("/dir/bar/baz")
+		self.assertEqual(self.fs.list_dir(),
+		    {})
